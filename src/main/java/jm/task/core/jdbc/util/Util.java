e3077bea09185b2bf51efbb25afb359319644d49
BlackAstra1008 <bush.vk@yandex.ru>
2025-01-15 13:00:09 +0300

    first

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    String url = "jdbc:mysql://localhost:3306/firstdb";
+    String user = "root";
+    String password = "qwerty";
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            connection = DriverManager.getConnection(url, user, password);
+
+        } catch (SQLException e) {
+            e.getMessage();
+            System.out.println("Connection Failed");
+        }
+
+        return connection;
+    }
 }
